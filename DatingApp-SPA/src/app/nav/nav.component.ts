a3336d7a01eebcf41f2215772286b735fb53ddc9
Peter Tejeda <Teyhey@users.noreply.github.com>
2020-07-09 14:52:35 -0500

    Successful implementation of Auth0 JWT tokens

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -10,7 +10,7 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) {}
+  constructor(public authService: AuthService, private alertify: AlertifyService) {}
 
   ngOnInit(): void {}
 
@@ -26,8 +26,7 @@ export class NavComponent implements OnInit {
   }
 
   loggedIn(): any {
-    const token = localStorage.getItem('token');
-    return !!token;
+    return this.authService.loggedIn();
   }
 
   logout(): void {
