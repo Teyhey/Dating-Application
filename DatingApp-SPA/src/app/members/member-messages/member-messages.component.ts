233ee463ff3f5a7d93065624d24fd6c9101bcbdb
Peter Tejeda <Teyhey@users.noreply.github.com>
2020-09-16 20:33:37 -0500

    Got messages being read after being viewed working again
    This was so annoying and was the reason why I never finished this project. Now I've figured it out after giving it a break for a while (3 months lol).

--- DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
+++ DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
@@ -26,12 +26,30 @@ export class MemberMessagesComponent implements OnInit {
   }
 
   loadMessages(): void {
-   this.userService.getMessageThread(this.authService.decodedToken.nameid, this.recipientId)
-   .subscribe(messages => {
-     this.messages = messages;
-   }, error => {
-     this.alertify.error(error);
-   });
+    const currentUserId = +this.authService.decodedToken.nameid;
+    this.userService
+      .getMessageThread(this.authService.decodedToken.nameid, this.recipientId)
+      .pipe(
+        tap((messages: Message[]) => {
+          // tslint:disable-next-line: prefer-for-of
+          for (let i = 0; i < messages.length; i++) {
+            if (
+              messages[i].isRead === false &&
+              messages[i].recipientId === currentUserId
+            ) {
+              this.userService.markAsRead(currentUserId, messages[i].id);
+            }
+          }
+        })
+      )
+      .subscribe(
+        (messages) => {
+          this.messages = messages;
+        },
+        (error) => {
+          this.alertify.error(error);
+        }
+      );
   }
 
   sendMessage(): any {
