f362f37882eab2f08ff78704953ff211a276a3cd
Peter Tejeda <Teyhey@users.noreply.github.com>
2020-09-16 20:12:50 -0500

    Holy smokes, messaging is working again.
    Found the bug with the messages not showing in chat. Now i just gotta work from here and get all the features complete. Also have to radically change the UI to impress some folks ;)

--- DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
+++ DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
@@ -26,29 +26,12 @@ export class MemberMessagesComponent implements OnInit {
   }
 
   loadMessages(): void {
-    const currentUserId = +this.authService.decodedToken.nameid;
-    this.userService
-      .getMessageThread(this.authService.decodedToken.nameid, this.recipientId)
-      .pipe(
-        tap((messages) => {
-          for (let i = 0; i < messages.length; i++) {
-            if (
-              messages[i].isRead === false &&
-              messages[i].recipientId === currentUserId
-            ) {
-              this.userService.markAsRead(currentUserId, messages[i].id);
-            }
-          }
-        })
-      )
-      .subscribe(
-        (messages) => {
-          this.messages = messages;
-        },
-        (error) => {
-          this.alertify.error(error);
-        }
-      );
+   this.userService.getMessageThread(this.authService.decodedToken.nameid, this.recipientId)
+   .subscribe(messages => {
+     this.messages = messages;
+   }, error => {
+     this.alertify.error(error);
+   });
   }
 
   sendMessage(): any {
